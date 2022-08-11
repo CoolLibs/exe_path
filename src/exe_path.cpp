7b5a7a733b4730b59e4b63cf25b6670130c516b5
badbois <Jordan.roberty@outlook.fr>
2022-08-11 11:16:10 +0200

    🚨 [exe_path.cpp] Add include sys/syslimits.h to get PATH_MAX

--- src/exe_path.cpp
+++ src/exe_path.cpp
@@ -29,12 +29,14 @@ static std::filesystem::path executable_path_impl()
 
 #if defined(__APPLE__)
 #include <mach-o/dyld.h>
+#include <sys/syslimits.h>
 static std::filesystem::path executable_path_impl()
 {
     char raw_path_name[PATH_MAX];
     char real_path_name[PATH_MAX];
     auto raw_path_size = static_cast<uint32_t>(sizeof(raw_path_name));
-    if (!_NSGetExecutablePath(raw_path_name, &raw_path_size)) {
+    if (!_NSGetExecutablePath(raw_path_name, &raw_path_size))
+    {
         realpath(raw_path_name, real_path_name);
     }
     return real_path_name;
