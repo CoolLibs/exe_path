4af9e16af2e7c0e3fd8565199b6801aa398b60c9
Jules Fouchy <jules.fouchy@ntymail.com>
2023-07-25 20:50:12 +0200

    🐛 [user_data()] Fix MacOS implementation

--- src/exe_path.cpp
+++ src/exe_path.cpp
@@ -56,7 +56,6 @@ static auto user_data_path_impl() -> std::filesystem::path
 #endif
 
 #if defined(__APPLE__)
-#include <CoreFoundation/CoreFoundation.h>
 #include <mach-o/dyld.h>
 #include <sys/syslimits.h>
 
@@ -74,24 +73,9 @@ static auto executable_path_impl() -> std::filesystem::path
 
 static auto user_data_path_impl() -> std::filesystem::path
 {
-    CFURLRef home_url = CFCopyHomeDirectoryURL();
-    if (home_url == nullptr)
+    const char* path = std::getenv("HOME");
+    if (path == nullptr)
         return "";
-
-    auto path = std::filesystem::path{""};
-
-    CFStringRef home_path = CFURLCopyFileSystemPath(home_url, kCFURLPOSIXPathStyle);
-    if (home_path != nullptr)
-    {
-        char path_buffer[PATH_MAX];
-        if (CFStringGetCString(home_path, path_buffer, sizeof(path_buffer), kCFStringEncodingUTF8))
-        {
-            path = path_buffer;
-        }
-        CFRelease(home_path);
-    }
-    CFRelease(home_url);
-
     return path;
 }
 
